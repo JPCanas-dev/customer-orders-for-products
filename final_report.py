16db72c36ad3879e53ec714b485745cf49084b49
4ndr3s-00 <andresteheranr@gmail.com>
2026-03-17 10:19:19 -0500

    final_report_test

---
+++ final_report.py
@@ -0,0 +1,29 @@
+def final_report(orders):
+    total_orders = 0
+    total_income = 0
+    clients = {}
+    products = {}
+    for order_id, order in orders.item():
+        total_orders += 1
+        total_income += order["tot:al"]
+
+        client = order["client"]
+        product = order["product"]
+        quantity = order["quantity"]
+
+        if client not in clients:
+            clients[client] = 0
+            clients[client] += order["total"]
+
+        if product not in products:
+            products[product] = 0
+            products[product] += quantity
+
+    report = (
+        total_orders,
+        total_income,
+        clients,
+        products
+        
+    )
+    return report
