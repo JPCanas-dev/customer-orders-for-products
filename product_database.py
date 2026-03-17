d668d97e27559efefc5b978fab8ac25c93c97d1c
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-17 10:25:23 -0500

    creacion de la base de datos de los productos

---
+++ product_database.py
@@ -0,0 +1,62 @@
+def products ():
+    product_1 = {
+    "id": 1001,
+    "name": "OKCO Phone",
+    "price_cop": 1200000
+    }
+
+    product_2 = {
+    "id": 1002,
+    "name": "OKCO Phone Lite",
+    "price_cop": 800000
+    }
+
+    product_3 = {
+    "id": 1003,
+    "name": "OKCO Phone Pro",
+    "price_cop": 1800000
+    }
+
+    product_4 = {
+    "id": 1004,
+    "name": "OKCO Tab",
+    "price_cop": 900000
+    }
+
+    product_5 = {
+    "id": 1005,
+    "name": "OKCO Book",
+    "price_cop": 2500000
+    }
+
+    product_6 = {
+    "id": 1006,
+    "name": "OKCO Buds",
+    "price_cop": 150000
+    }
+
+    product_7 = {
+    "id": 1007,
+    "name": "OKCO Watch",
+    "price_cop": 600000
+    }
+
+    product_8 = {
+    "id": 1008,
+    "name": "OKCO Home Hub",
+    "price_cop": 300000
+    }
+
+    product_9 = {
+    "id": 1009,
+    "name": "OKCO Play",
+    "price_cop": 700000
+    }
+
+    product_10 = {
+    "id": 1010,
+    "name": "OKCO Cloud (suscripción mensual)",
+    "price_cop": 20000
+    }
+    
+    return product_1, product_2, product_3, product_4, product_5, product_6, product_7, product_8, product_9, product_10
