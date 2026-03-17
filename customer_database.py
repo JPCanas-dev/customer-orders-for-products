daa4e090bd06032379be86a53536ac82eb8bd55c
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-17 10:24:53 -0500

    creacion de la base de datos de los clientes

---
+++ customer_database.py
@@ -0,0 +1,32 @@
+def customers():
+    c1 = {
+        "id": 1102,
+        "name": "NovaTech Solutions",
+        "email": "contacto@novatech1102.com"
+    }
+
+    c2 = {
+        "id": 2235,
+        "name": "DigitalCore Systems",
+        "email": "ventas@digitalcore2235.com"
+    }
+
+    c3 = {
+        "id": 3478,
+        "name": "FutureWave Tech",
+        "email": "info@futurewave3478.com"
+    }
+
+    c4 = {
+        "id": 4591,
+        "name": "SmartLink Industries",
+        "email": "soporte@smartlink4591.com"
+    }
+
+    c5 = {
+        "id": 5684,
+        "name": "NexaVision Corp",
+        "email": "contacto@nexavision5684.com"
+    }
+
+    return c1, c2, c3, c4, c5
