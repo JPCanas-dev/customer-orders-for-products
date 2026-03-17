95156bda84b6f8bcb30e552b4eefc4583096c53c
estebanp26 <estejps2607@gmail.com>
2026-03-17 09:58:25 -0500

    primera parte

---
+++ products.py
@@ -0,0 +1,8 @@
+def register_products(product_id, product_name, unit_price):
+
+    product_id(input("Enter the product id: "))
+    product_name(input("Enter the product name: "))
+    unit_price(input("Enter unit price: "))
+    
+
+
