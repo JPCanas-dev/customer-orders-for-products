67c94232b28425f4229aa3c151ca89fe624f3049
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-18 06:59:23 -0500

    actualization of customer_database.py

--- customer_database.py
+++ customer_database.py
@@ -1,32 +1,9 @@
-def customers():
-    c1 = {
-        "id": 1102,
-        "name": "NovaTech Solutions",
-        "email": "contacto@novatech1102.com"
-    }
-
-    c2 = {
-        "id": 2235,
-        "name": "DigitalCore Systems",
-        "email": "ventas@digitalcore2235.com"
-    }
-
-    c3 = {
-        "id": 3478,
-        "name": "FutureWave Tech",
-        "email": "info@futurewave3478.com"
-    }
-
-    c4 = {
-        "id": 4591,
-        "name": "SmartLink Industries",
-        "email": "soporte@smartlink4591.com"
-    }
-
-    c5 = {
-        "id": 5684,
-        "name": "NexaVision Corp",
-        "email": "contacto@nexavision5684.com"
-    }
-
-    return c1, c2, c3, c4, c5
+customers = {
+    1102: {"id": 1102, "name": "NovaTech Solutions", "email": "contacto@novatech1102.com"},
+    2235: {"id": 2235, "name": "DigitalCore Systems", "email": "ventas@digitalcore2235.com"},
+    3478: {"id": 3478, "name": "FutureWave Tech", "email": "info@futurewave3478.com"},
+    4591: {"id": 4591, "name": "SmartLink Industries", "email": "soporte@smartlink4591.com"},
+    5684: {"id": 5684, "name": "NexaVision Corp", "email": "contacto@nexavision5684.com"},
+}
+#Diccionario que actua como base de 
+# datos que contiene toda la informacion de los 5 clientes
