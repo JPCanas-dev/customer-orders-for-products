105f309a739b00142522f9feff215c62202ed67b
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-19 14:33:24 -0500

    correcion de la base de datos

--- customer_database.py
+++ customer_database.py
@@ -1,9 +1,9 @@
 customers = {
-    1102: {"id": 1102, "name": "NovaTech Solutions", "email": "contacto@novatech1102.com"},
-    2235: {"id": 2235, "name": "DigitalCore Systems", "email": "ventas@digitalcore2235.com"},
-    3478: {"id": 3478, "name": "FutureWave Tech", "email": "info@futurewave3478.com"},
-    4591: {"id": 4591, "name": "SmartLink Industries", "email": "soporte@smartlink4591.com"},
-    5684: {"id": 5684, "name": "NexaVision Corp", "email": "contacto@nexavision5684.com"},
+    1102: ("NovaTech Solutions", "contacto@novatech1102.com"),
+    2235: ("DigitalCore Systems", "ventas@digitalcore2235.com"),
+    3478: ("FutureWave Tech", "info@futurewave3478.com"),
+    4591: ("SmartLink Industries", "soporte@smartlink4591.com"),
+    5684: ("NexaVision Corp", "contacto@nexavision5684.com"),
 }
-#Diccionario que actua como base de 
-# datos que contiene toda la informacion de los 5 clientes
+#A dictionary that stores tuples containing information about clients and functions as a database
+
