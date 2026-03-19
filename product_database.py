04bd6e6e46d5e6c6e0a757e51ebd858e01087c73
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-19 14:34:06 -0500

    correcicion de la base de datos de los proudctos

--- product_database.py
+++ product_database.py
@@ -1,12 +1,13 @@
 products = {
-    "1001": {"id": 1001, "name": "OKCO Phone","price_cop": 1200000},
-    "1002": {"id": 1002, "name": "OKCO Phone Lite","price_cop":  800000},
-    "1003": {"id": 1003, "name": "OKCO Phone Pro","price_cop": 1800000},
-    "1004": {"id": 1004, "name": "OKCO Tab","price_cop":  900000},
-    "1005": {"id": 1005, "name": "OKCO Book","price_cop": 2500000},
-    "1006": {"id": 1006, "name": "OKCO Buds", "price_cop":  150000},
-    "1007": {"id": 1007, "name": "OKCO Watch", "price_cop":  600000},
-    "1008": {"id": 1008, "name": "OKCO Home Hub","price_cop":  300000},
-    "1009": {"id": 1009, "name": "OKCO Play", "price_cop":  700000},
-    "1010": {"id": 1010, "name": "OKCO Cloud (suscripcion mensual)", "price_cop":   20000},
+    1001: ("OKCO Phone", 1200000),
+    1002: ("OKCO Phone Lite", 800000),
+    1003: ("OKCO Phone Pro", 1800000),
+    1004: ("OKCO Tab", 900000),
+    1005: ("OKCO Book", 2500000),
+    1006: ("OKCO Buds", 150000),
+    1007: ("OKCO Watch", 600000),
+    1008: ("OKCO Home Hub", 300000),
+    1009: ("OKCO Play", 700000),
+    1010: ("OKCO Cloud (monthly sub)", 20000),
 }
+# A dictionary that stores tuples containing product information, acting as a database
