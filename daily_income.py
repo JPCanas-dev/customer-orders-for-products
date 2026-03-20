833a43681825e5b09be16e1b65512bd0ecbf7beb
estebanp26 <estejps2607@gmail.com>
2026-03-20 06:24:02 -0500

    Delete daily_income.py

--- daily_income.py
+++
@@ -1,8 +0,0 @@
-def calculate_daily_income(orders):
-    total_income = 0
-
-    for order_id in orders:
-        order = orders[order_id]
-        total_income += order["total"]
-
-    return total_income
